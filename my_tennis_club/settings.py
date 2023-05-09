ca81c3d44991e3608ca33a52ac8f1ef7fcafa3b8
nien lin hsueh <nlhsueh@gmail.com>
2023-05-09 11:40:19 +0800

    display data

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '0xxz(iu$-a90+!l9(9_c0z_-iy!oh117j7jg7$-lq6i7a-qvkh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
